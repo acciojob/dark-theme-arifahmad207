7cbb4be63d7b31c9b0bbe4389c8af5e70fda3979
MD ARIF <arifahmad207@gmail.com>
2023-02-20 13:17:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 //your code here
 
+function swapTheme() {
+    var app = document.getElementById("app");
+    var swap = document.getElementById("swap");
+    if (app.classList.contains("day")) {
+        app.classList.remove("day");
+        app.classList.add("night");
+        swap.classList.remove("button_day");
+        swap.classList.add("button_night");
+    } else {
+        app.classList.remove("night");
+        app.classList.add("day");
+        swap.classList.remove("button_night");
+        swap.classList.add("button_day");
+    }
+}
